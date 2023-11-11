1075ccf311f19679b98b7ec93c6a5dc3a3b5481c
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-11 00:00:49 -0800

    latet

--- app/component/HomeCom/SectionFour.tsx
+++ app/component/HomeCom/SectionFour.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import user from './user.png'
-import Image from 'next/image'
+// import React from 'react'
+// import user from './user.png'
+// import Image from 'next/image'
 
-const SectionFour = () => {
-  return (
-    <div>
-        <section className='myBg-gradient text-center py-28 max-sm:p-8'>
-            <p className='text-2xl max-sm:text-base'>Testimonials</p>
-            <h2 className='text-4xl pt-5 pb-5 max-sm:text-2xl'>What people say about Us.</h2>
-            <p className='text-xl pb-6 max-sm:text-sm '>
-                “On the Windows talking painted pasture yet its express parties 
-                use. <br /> Sure last upon he same as knew next. Of believed or diverted no.”
-            </p>
-            <div className='flex m-auto text-center justify-center gap-4'>
-              <Image src={user} width={50} height={50} alt='user'/>
-              <div className='text-left'>
-                <h3>Mike taylor</h3>
-                <h3>Lahore, Pakistan</h3>
-              </div>
-            </div>
-        </section>
-    </div>
-  )
-}
+// const SectionFour = () => {
+//   return (
+//     <div>
+//         <section className='myBg-gradient text-center py-28 max-sm:p-8'>
+//             <p className='text-2xl max-sm:text-base'>Testimonials</p>
+//             <h2 className='text-4xl pt-5 pb-5 max-sm:text-2xl'>What people say about Us.</h2>
+//             <p className='text-xl pb-6 max-sm:text-sm '>
+//                 “On the Windows talking painted pasture yet its express parties 
+//                 use. <br /> Sure last upon he same as knew next. Of believed or diverted no.”
+//             </p>
+//             <div className='flex m-auto text-center justify-center gap-4'>
+//               <Image src={user} width={50} height={50} alt='user'/>
+//               <div className='text-left'>
+//                 <h3>Mike taylor</h3>
+//                 <h3>Lahore, Pakistan</h3>
+//               </div>
+//             </div>
+//         </section>
+//     </div>
+//   )
+// }
 
-export default SectionFour
+// export default SectionFour
